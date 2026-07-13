34d204e31f39b760019955f6109c78fec612dba4
MD Jobayer <allmgroup20@gmail.com>
2026-07-13 10:52:51 +0600

    feat: add all 122 PHP courses with categories, replace platforms/trainers, remove 99 taka CTAs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import LiveNotificationBar from "@/components/home/LiveNotificationBar";
 import HeroSection from "@/components/home/HeroSection";
 import StatsCounter from "@/components/home/StatsCounter";
-import ProblemSection from "@/components/home/ProblemSection";
-import PriceAnchorSection from "@/components/home/PriceAnchorSection";
 import HowItWorks from "@/components/home/HowItWorks";
 import PlatformShowcase from "@/components/home/PlatformShowcase";
 import CourseCatalog from "@/components/home/CourseCatalog";
@@ -13,7 +11,6 @@ import SalaryTable from "@/components/home/SalaryTable";
 import PaymentGallery from "@/components/home/PaymentGallery";
 import Testimonials from "@/components/home/Testimonials";
 import FAQSection from "@/components/home/FAQSection";
-import CheckoutCta from "@/components/home/CheckoutCta";
 import TrustSection from "@/components/home/TrustSection";
 import TrainerPhotoGrid from "@/components/home/TrainerPhotoGrid";
 import PlatformLogoGrid from "@/components/home/PlatformLogoGrid";
@@ -25,8 +22,6 @@ export default function HomePage() {
       <HeroSection />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 space-y-6 pb-8 md:pb-12 -mt-8 relative z-20">
         <StatsCounter />
-        <PriceAnchorSection />
-        <ProblemSection />
         <HowItWorks />
         <PlatformShowcase />
         <CourseCatalog />
@@ -37,7 +32,6 @@ export default function HomePage() {
         <TrainerPhotoGrid />
         <Testimonials />
         <FAQSection />
-        <CheckoutCta />
         <TrustSection />
       </div>
     </div>
